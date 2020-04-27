3c9c8f3f4515584d4c28ce6cb7a078aebbf59a7f
niharikak101 <niharikak101@gmail.com>
2020-04-27 02:37:00 +0100

    update server pointer

--- src/constants/websocket.js
+++ src/constants/websocket.js
@@ -1,7 +1,7 @@
 // Shared web socket
 import io from 'socket.io-client'
 
-const ENDPOINT = 'http://localhost:5000/'
+const ENDPOINT = 'https://name-place-animal-thing.herokuapp.com'
 const socket = io(ENDPOINT);
 
 export { socket }
