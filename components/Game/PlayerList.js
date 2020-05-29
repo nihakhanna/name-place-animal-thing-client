43d33a8b9bea4f4c170f3d3e5ab9bf3943ff3fcc
niharikak101 <niharikak101@gmail.com>
2020-05-29 12:31:47 +0100

    copy to clipboard

--- components/Game/PlayerList.js
+++ components/Game/PlayerList.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { keyframes } from 'styled-components';
 import theme, { avatars } from '../../constants/theme'
 import { FlexContainer } from '../StyledComponents'
 
+import copy from 'clipboard-copy';
+
 const ActivePlayers = ({ users, gameStarted, code }) => {
   const width = gameStarted ? 30 : 60;
   const height = width;
+  const [copiedString, setCopiedString] = useState('Copy')
 
   return <>
-    {!gameStarted && <div style={{ textAlign: "center", padding: "20px 20px 20px 20px" }}><p>
+    {!gameStarted && <div style={{ textAlign: "center", padding: "20px 20px 20px 20px" }}><div style={{ padding: "10px" }}>
       Your Game Code is:
-            <GameCode>{` `}{code}</GameCode>. Send it to your friends to start the game!
-          </p>
+      <div>
+        <GameCode>{` `}{code}</GameCode>
+        <CopyButton disabled={copiedString === "Copied!"} onClick={() => {
+          gtag("event", "copy_to_clipboard");
+          copy(code).then(() => setCopiedString('Copied!'))
+        }}>{copiedString}</CopyButton>
+      </div>
+      Send it to your friends to start the game!
+    </div>
     </div>}
     <Container>
       {!gameStarted && <h1>Who's Playing?</h1>}
@@ -39,6 +49,18 @@ const flicker = keyframes`
     transform: rotate(8deg);
   }
 `;
+
+const CopyButton = styled.button`
+  margin: 5px;
+  margin-left: 10px;
+  padding: 5px 10px;
+  font-size: 20px;
+  color: white;
+  border: none;
+  cursor: ${props => props.disabled ? 'not-allowed' : 'pointer'};
+  background-color: ${props => props.disabled ? 'grey' : theme.colors.blue};
+  font-family: ${theme.font};
+  `
 
 
 const GameCode = styled.span`
