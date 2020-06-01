a1b9cc6df1645d2eda04fe7f5ac1adde096f6707
niharikak101 <niharikak101@gmail.com>
2020-06-01 20:03:30 +0100

    update privacy policy

--- pages/privacy.js
+++ pages/privacy.js
@@ -14,19 +14,110 @@ const Privacy = () => <>
   </Head>
   <Container>
     <h1 style={{ fontSize: "3em" }}>Privacy Policy</h1>
-    <h2>This policy applies to all information collected or submitted on our website and our apps for Android and any other devices and platforms.</h2>
+    <h2>
+      Pegasus Games built the Name, Place, Animal, Thing! app as
+      a free app. This game is provided by
+      Pegasus Games at no cost and is intended for use as
+      is.
+               </h2><h2>
+      This page is used to inform visitors regarding our
+      policies with the collection, use, and disclosure of Personal
+      Information if anyone decided to use our game.
+               </h2><h2>
+      If you choose to use our game, then you agree to
+      the collection and use of information in relation to this
+      policy. The Personal Information that we collect is
+      used for providing and improving the game. we will not use or share your information with
+      anyone except as described in this Privacy Policy.
+               </h2><h2>
+      The terms used in this Privacy Policy have the same meanings
+      as in our Terms and Conditions, which is accessible at
+      Name, Place, Animal, Thing! unless otherwise defined in this Privacy Policy.
+               </h2> <h1>Information Collection and Use</h1><h2>
+      For a better experience, while using our game, we
+      may require you to provide us with certain personally
+      identifiable information. The information that
+      we request will be retained by us and used as described in this privacy policy.
+               </h2> <div><h2>
+      The app does use third party services that may collect
+      information used to identify you.
+                 </h2><h2>
+        Link to privacy policy of third party service providers used
+        by the app
+                 </h2> <ul><li><a href="https://www.google.com/policies/privacy/" target="_blank">Google Play Services</a></li><li><a href="https://support.google.com/admob/answer/6128543?hl=en" target="_blank">AdMob</a></li><li><a href="https://firebase.google.com/policies/analytics" target="_blank">Google Analytics for Firebase</a></li></ul></div>
 
-    <h1>Information we collect</h1>
-    <h2>We do not store any persistent information about you.</h2>
-
-    <h1>Analytics</h1>
-    <h2>Our app collects aggregate, anonymous statistics, such as the percentage of users who use particular features, to improve the app. We may partner with third party vendors such as Google Analytics to do so.</h2>
-
-    <h1>Use of your information</h1>
-    <ul>
-      <li style={{ fontSize: '1.5em' }}>Compile anonymous statistical data and analysis for use internally or with third parties.</li>
-      <li style={{ fontSize: '1.5em' }}>Request feedback and contact you about your use of the Site and our mobile application</li>
-    </ul>
+    <h1>Log Data</h1><h2>
+      we want to inform you that whenever you
+      use our Service, in a case of an error in the app
+      we collect data and information (through third party
+      products) on your phone called Log Data. This Log Data may
+      include information such as your device Internet Protocol
+      (“IP”) address, device name, operating system version, the
+      configuration of the app when utilizing our Service,
+      the time and date of your use of the Service, and other
+      statistics.
+               </h2> <h1>Cookies</h1><h2>
+      Cookies are files with a small amount of data that are
+      commonly used as anonymous unique identifiers. These are sent
+      to your browser from the websites that you visit and are
+      stored on your device's internal memory.
+               </h2><h2>
+      This Service does not use these “cookies” explicitly. However,
+      the app may use third party code and libraries that use
+      “cookies” to collect information and improve their services.
+      You have the option to either accept or refuse these cookies
+      and know when a cookie is being sent to your device. If you
+      choose to refuse our cookies, you may not be able to use some
+      portions of this Service.
+               </h2> <h1>Service Providers</h1><h2>
+      we may employ third-party companies and
+      individuals due to the following reasons:
+               </h2> <ul>
+      <li><h2>To facilitate our Service;</h2></li>
+      <li><h2>To provide the Service on our behalf;</h2></li>
+      <li><h2>To perform Service-related services; or</h2></li>
+      <li><h2>To assist us in analyzing how our Service is used.</h2></li></ul>
+    <h2>
+      we want to inform users of this Service
+      that these third parties have access to your Personal
+      Information. The reason is to perform the tasks assigned to
+      them on our behalf. However, they are obligated not to
+      disclose or use the information for any other purpose.
+               </h2> <h1>Security</h1><h2>
+      we value your trust in providing us your
+      Personal Information, thus we are striving to use commercially
+      acceptable means of protecting it. But remember that no method
+      of transmission over the internet, or method of electronic
+      storage is 100% secure and reliable, and we cannot
+      guarantee its absolute security.
+               </h2> <h1>Links to Other Sites</h1><h2>
+      This Service may contain links to other sites. If you click on
+      a third-party link, you will be directed to that site. Note
+      that these external sites are not operated by us.
+      Therefore, we strongly advise you to review the
+      Privacy Policy of these websites. we have
+      no control over and assume no responsibility for the content,
+      privacy policies, or practices of any third-party sites or
+      services.
+               </h2> <h1>Children’s Privacy</h1><h2>
+      These Services do not address anyone under the age of 13.
+      we do not knowingly collect personally
+      identifiable information from children under 13. In the case
+      we discover that a child under 13 has provided
+      us with personal information, we immediately
+      delete this from our servers. If you are a parent or guardian
+      and you are aware that your child has provided us with
+      personal information, please contact us so that
+      we will be able to do necessary actions.
+               </h2> <h1>Changes to This Privacy Policy</h1><h2>
+      we may update our Privacy Policy from
+      time to time. Thus, you are advised to review this page
+      periodically for any changes. we will
+      notify you of any changes by posting the new Privacy Policy on
+      this page.
+               </h2><h2>This policy is effective as of 2020-06-01</h2> <h1>Contact Us</h1><h2>
+      If you have any questions or suggestions about our
+                  Privacy Policy, do not hesitate to contact us at contact@nameplaceanimalthing.online.</h2>
   </Container>
 </>
 
