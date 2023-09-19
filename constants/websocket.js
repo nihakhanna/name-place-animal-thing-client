253b86a62963c83455d9bda804b325a12bf2d664
nihakhanna <niharikak101@gmail.com>
2023-09-19 17:38:59 +0100

    update server

--- constants/websocket.js
+++ constants/websocket.js
@@ -2,7 +2,7 @@
 import io from "socket.io-client";
 
 // const ENDPOINT = 'http://localhost:5000/'
-const ENDPOINT = "https://web-production-a3bc.up.railway.app/";
+const ENDPOINT = "name-place-animal-thing-server.vercel.app";
 const socket = io(ENDPOINT);
 
 export { socket };
