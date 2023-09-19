492f84d0dcd8bbde2f4d73b2bf97beaa96fa04b1
nihakhanna <niharikak101@gmail.com>
2023-09-19 18:33:31 +0100

    back to heroku

--- constants/websocket.js
+++ constants/websocket.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 
 // const ENDPOINT = 'http://localhost:5000/'
 const ENDPOINT =
-  "https://name-place-animal-thing-server-rh4o6cc4z-nihakhanna.vercel.app/";
+  "https://name-place-animal-thing-server-60a49bfac610.herokuapp.com/";
 const socket = io(ENDPOINT);
 
 export { socket };
